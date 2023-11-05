62f5be7d3b7a6296abb847f051a838983e09c717
Igor Drozdov <igor_drozdov@epam.com>
2023-11-05 22:28:32 +0100

    Movie details routing

--- src/components/MovieListPage/MovieListPage.tsx
+++ src/components/MovieListPage/MovieListPage.tsx
@@ -1,101 +1,44 @@
 import './MovieListPage.css';
 import '../../colors.css';
 
-import { useEffect, useState } from 'react';
+import {useState} from 'react';
 
 import GenreSelect from '../GenreSelect/GenreSelect';
 import SortControl from '../SortControl/SortControl';
 import MovieTile from '../MovieTile/MovieTile';
 import EditMovieDialog from '../EditMovieDialog/EditMovieDialog';
-import MovieDetails from '../MovieDetails/MovieDetails';
-import SearchForm from '../SearchForm/SearchForm';
 
-import { getMovies } from '../../services/api.service';
 
-import { Movie } from '../../models/movie';
+import {Movie} from '../../models/movie';
 
-import { GENRES } from '../../consts';
-import {useSearchParams} from "react-router-dom";
+import {GENRES} from '../../consts';
+import {Outlet, useLoaderData, useSearchParams} from "react-router-dom";
 
 const MovieListPage = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const activeGenre = searchParams.get('genre') as string;
 
-    const [searchQuery, setSearchQuery] = useState<string>('');
+    const {movieList} = useLoaderData() as { movieList: Array<Movie> };
+
     const [sortCriterion, setSortCriterion] = useState<'releaseDate' | 'title'>('releaseDate');
-    const [movieList, setMovieList] = useState<Movie[]>([] as Movie[]);
-    const [showDetailContainer, setShowDetailContainer] = useState<boolean>(false);
-    const [selectedMovie, setSelectedMovie] = useState<Movie>({} as Movie);
+    const [selectedMovie,] = useState<Movie>({} as Movie);
     const [showEditDialog, setShowEditDialog] = useState<boolean>(false);
 
-    useEffect(() => {
-        void getMovies(searchQuery, sortCriterion, activeGenre).then((data) => {
-            const formatMovieList: Movie[] = data.data.map((movieApi) => ({
-                id: movieApi.id,
-                movieName: movieApi.title,
-                releaseYear: +movieApi.release_date.split('-')[0],
-                genres: movieApi.genres,
-                voteAverage: movieApi.vote_average,
-                description: movieApi.overview,
-                duration: movieApi.runtime,
-                imageUrl: movieApi.poster_path,
-            }));
-            setMovieList(formatMovieList);
-        });
-    }, [searchQuery, sortCriterion, activeGenre]);
-
-    const searchMovieByName = (name: string): void => {
-        setSearchQuery(name);
-    };
 
     const showGenreMovies = (selectedGenre: string): void => {
         setSearchParams({genre: selectedGenre})
     };
     const openDetailInfo = (openMovieId: number): void => {
-        const selectedMovie: Movie = movieList.filter((movie) => movie.id === openMovieId)[0];
-
-        setSelectedMovie({ ...selectedMovie });
-
-        toggleDetailedContainer(true);
-        window.scrollTo(0, 0);
-        // setShowEditDialog(true);
+        console.log(openMovieId)
     };
-
-    const toggleDetailedContainer = (show?: boolean) =>
-        setShowDetailContainer((prevState) => (show === undefined ? !prevState : show));
 
     const sortMoviesBy = (sortedBy: 'releaseDate' | 'title'): void => {
         setSortCriterion(sortedBy);
     };
 
-    const HeaderContainer = () => {
-        return (
-            <>
-                {showDetailContainer ? (
-                    <>
-                        <span
-                            className='exitButton'
-                            onClick={() => toggleDetailedContainer()}
-                        ></span>
-                        <MovieDetails {...selectedMovie} />
-                    </>
-                ) : (
-                    <div className='headerContainer'>
-                        <div className='blurContainer'></div>
-                        <h1 className='headerTitle'>FIND YOUR MOVIE</h1>
-                        <SearchForm
-                            initialValue={searchQuery}
-                            searchMovie={(name: string) => searchMovieByName(name)}
-                        />
-                    </div>
-                )}
-            </>
-        );
-    };
-
     return (
         <>
-            <HeaderContainer />
+            <Outlet/>
             <main className='moviesListPage'>
                 <div className='sortFilterLine'>
                     <GenreSelect
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider, useLoaderData} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import SearchForm from "./components/SearchForm/SearchForm";
+import {getMovies} from "./services/api.service";
+import {Movie, MovieAPI} from "./models/movie";
+
+const MD = () => {
+    const selectedMovie = (useLoaderData() as {movie: Movie}).movie
+    console.log('movie details', selectedMovie);
+    return <>
+                        <span
+                            className='exitButton'
+                            // onClick={() => toggleDetailedContainer()}
+                        />
+        <MovieDetails {...selectedMovie} />
+    </>;
+}
+
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />,
+        element: <App/>,
+        loader: async ({request}) => {
+            const url = new URL(request.url);
+            const genre = url.searchParams.get("genre") as string; // todo other search params
+            const movieList = await getMovies('', 'releaseDate', genre).then((data) => {
+                return data.data.map((movieApi) => ({
+                    id: movieApi.id,
+                    movieName: movieApi.title,
+                    releaseYear: +movieApi.release_date.split('-')[0],
+                    genres: movieApi.genres,
+                    voteAverage: movieApi.vote_average,
+                    description: movieApi.overview,
+                    duration: movieApi.runtime,
+                    imageUrl: movieApi.poster_path,
+                }));
+            });
+            return {movieList}
+        },
+        children: [
+            {
+                path: ':movieId',
+                element: <MD/>,
+                loader: async ({params: {movieId}}) => {
+                    console.log(movieId);
+                    const movies = await getMovies('', 'releaseDate', 'all')
+                    const movieApi = movies.data.find(m => m.id === Number(movieId)) as MovieAPI
+                    return {
+                        movie: {
+                            id: movieApi.id,
+                            movieName: movieApi.title,
+                            releaseYear: +movieApi.release_date.split('-')[0],
+                            genres: movieApi.genres,
+                            voteAverage: movieApi.vote_average,
+                            description: movieApi.overview,
+                            duration: movieApi.runtime,
+                            imageUrl: movieApi.poster_path,
+                        }
+                    };
+                }
+            },
+            {
+                path: '',
+                element: <div className='headerContainer'>
+                    <div className='blurContainer'></div>
+                    <h1 className='headerTitle'>FIND YOUR MOVIE</h1>
+                    <SearchForm
+                        initialValue='{searchQuery}'
+                        searchMovie={() => {
+                        }}
+                    />
+                </div>
+            }
+        ]
     },
 ]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <RouterProvider router={router}/>
     </React.StrictMode>,
 );
 
